2c2107162bd66073b6dc959fba860c7ecc917846
Abigail Kane <aekane403@gmail.com>
2018-07-30 14:25:14 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -5,7 +5,7 @@ class GardenGnome
   attr_accessor :name, :age, :gluten_allergy
   attr_reader :personality
 
-def initialize(personality= "evil", hat_color)
+def initialize(personality= "evil", hat_color = "red")
   @personality = personality
 end 
 end
